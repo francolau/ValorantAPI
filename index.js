1fc9f043e780a2fc97cabd469c4ea22fec139bb0
francolau <franco.lau.12@gmail.com>
2023-10-17 16:33:57 -0300

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./src/app.js')
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan')
 
 // const app = express();
@@ -10,6 +11,7 @@ app.set('json spaces', 2)
 
 //middlewres
 app.use(morgan('dev'));
+app.use(cors())
     //Tratar de enter solo textos y no nada pesado
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
